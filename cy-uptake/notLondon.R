7e47d4a1c75aa2fcdd9d30d674ea87fda58e1716
Robinlovelace <rob00x@gmail.com>
2014-05-12 07:53:53 +0100

    1st results in

---
+++ cy-uptake/notLondon.R
@@ -0,0 +1,22 @@
+# change in the rate of cycling outside london
+
+notLon <- lam[ lam$Region != "London", ]
+plot(notLon$pCycle01, notLon$pCycle) # make this plot nice! add regions!!!
+summary(notLon$pCycle)
+summary(notLon$pCycle01)
+sum(notLon$Cycle.y) / sum(notLon$Allm.y)
+sum(notLon$Cycle.x) / sum(notLon$Allm.x)
+notLon$Growth <- (notLon$pCycle / notLon$pCycle01  - 1) * 100
+notLon$Abs.Growth <- (notLon$pCycle - notLon$pCycle01) * 100
+
+# analysing the growth in cycling
+summary(notLon$Growth)
+summary(notLon$Abs.Growth)
+length(which(abs(notLon$Abs.Growth) < 1)) / nrow(notLon)
+cD <- which(notLon$Growth < 0) # cycling Down
+cU <- which(notLon$Growth > 0)
+length(cD)
+summary(notLon$Growth[cD])
+length(which(notLon$Growth < 0))
+summary(notLon$Growth[cU])
+hist(notLon$Growth)
